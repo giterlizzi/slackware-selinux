632f4ae0227e10616ea3806525d922b71cbce782
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2024-05-27 22:55:34 +0200

    First release

---
+++ README.md
@@ -0,0 +1,41 @@
+# SELinux for Slackware
+
+These are the hackings on SELinux integreation to Slackware Linux.
+To date it has just keep up with the -current development branch. 
+
+## About SELinux
+
+SELinux is a security enhancement to Linux which allows users and administrators more control over access control.
+
+Access can be constrained on such variables as which users and applications can access which resources. These resources may take the form of files. Standard Linux access controls, such as file modes (-rwxr-xr-x) are modifiable by the user and the applications which the user runs. Conversely, SELinux access controls are determined by a policy loaded on the system which may not be changed by careless users or misbehaving applications.
+
+SELinux also adds finer granularity to access controls. Instead of only being able to specify who can read, write or execute a file, for example, SELinux lets you specify who can unlink, append only, move a file and so on. SELinux allows you to specify access to many resources other than files as well, such as network resources and interprocess communication (IPC). 
+
+## Instruction
+
+Build and install/upgrade all SELinux packages:
+
+*NOTE* `audit` package is required for build SELinux.
+
+    cd selinux
+    sh selinux-build-all.sh
+
+## TODO
+
+* Optimize `selinux-policy` for Slackware Linux
+* Add all dependencies:
+  * `audit`
+* Rebuild some Slackware-core packages for SELinux
+  * `a/coreutils`
+  * `a/pam`
+  * `a/shadow`
+  * `a/sysvinit`
+  * `ap/sudo`
+  * `n/iproute2`
+  * `n/net-tools`
+
+## Copyright
+
+ - Copyright 2022-20124 © Giuseppe Di Terlizzi
+ - Slackware® is a Registered Trademark of Patrick Volkerding
+ - Linux is a Registered Trademark of Linus Torvalds
---
+++ selinux/selinux-build-all.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+
+# Copyright 2012  Patrick J. Volkerding, Sebeka, Minnesota, USA
+# All rights reserved.
+#
+# Copyright 2023-2024 Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+# All rights reserved.
+#
+# Based on the xfce-build-all.sh script by Patrick J. Volkerding
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+cd $(dirname $0) ; CWD=$(pwd)
+
+# Set to 1 if you'd like to install/upgrade package as they are built.
+# This is recommended.
+INST=${INST:-1}
+
+TMP=${TMP:-/tmp}
+TAG=${TAG:-_selinux}
+
+# Package           | Slackware category
+# ------------------|-------------------
+# libsepol          | l
+# libselinux        | l
+# secilc            | ap
+# checkpolicy       | ap
+# libsemanage       | l
+# semodule-utils    | ap
+# policycoreutils   | ap
+# setools           | ap (?)
+# selinux-dbus      | l  (?)
+# selinux-python    | l -or- ap (?)
+# selinux-gui       | ap
+# selinux-sandbox   | ap
+# mcstrans          | ap (?)
+# restorecond       | ap (?)
+# selinux-policy    | l
+
+for dir in \
+  libsepol \
+  libselinux \
+  secilc \
+  checkpolicy \
+  libsemanage \
+  semodule-utils \
+  policycoreutils \
+  setools \
+  selinux-dbus \
+  selinux-python \
+  selinux-gui \
+  selinux-sandbox \
+  mcstrans \
+  restorecond \
+  selinux-policy \
+  ; do
+
+  ( cd $dir || exit 1
+
+    package=$(basename $dir)
+
+    if [ ! -e slack-desc ]; then
+      touch /tmp/${package}.missing-slack-desc
+    fi
+
+    TAG=$TAG TMP=$TMP \
+    ./${package}.SlackBuild || ( touch /tmp/${package}.failed ; exit 1 ) || exit 1
+
+    if [ "$INST" = "1" ]; then
+      PACKAGE="$(ls -t $TMP/${package}-*txz | head -n 1)"
+      if [ -f $PACKAGE ]; then
+        upgradepkg --install-new --reinstall $PACKAGE
+      else
+        echo "Error:  package to upgrade "$PACKAGE" not found in $TMP"
+        exit 1
+      fi
+    fi
+
+  ) || exit 1
+
+done
